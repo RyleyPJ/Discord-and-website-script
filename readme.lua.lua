349c905d509295d88f20dfc5fb52ce61fde5e891
RyleyPJ <134054428+RyleyPJ@users.noreply.github.com>
2023-06-17 02:27:04 +0100

    Add files via upload

---
+++ readme.lua.lua
@@ -0,0 +1,12 @@
+If anyone does use this script i appreciate it!
+
+Anyways let me talk you through it, so you will install this into your resources folder and you will then start it in 
+your server CFG, you will then go into the help-c.lua and you will change the example to your discord and website link
+if you have one.
+
+If you have any trouble please join my new CodingClub discord: https://discord.gg/AFjfCcP6gj
+If you are interested in my server you can join it here: https://discord.gg/bHuuQvq2c9
+
+If you have any questions feel free to open a ticket on the CodingClub discord!
+
+Thank you again if you use this script!
